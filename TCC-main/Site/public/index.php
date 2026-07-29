b35c461b4c73cc06ec0fe3fa5f6d0e36345b5719
Pierrvp <raphaelpierrelima@gmail.com>
2026-07-29 16:24:22 -0300

    Add files via upload

---
+++ TCC-main/Site/public/index.php
@@ -0,0 +1,26 @@
+<?php
+require_once("Navbar.php");
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> RG Eats </title>
+</head>
+<body>
+    
+   
+    
+    <?php
+require_once("rodape.php");
+
+
+?>
+</body>
+</html>
---
+++ TCC-main/Site/public/rodape.php
@@ -0,0 +1,12 @@
+ <!-- Rodape -->
+<footer>
+        <div id="logoRodape" onclick="window.location=''">
+            <img src="../Imagens/Logo1(Sem%20Fundo).png" alt="Logo RG Eats">
+            <h3>RG Eats</h3>
+        </div>
+        <div class="Texto">
+            Conteúdo desenvolvido pelo Professor José Victor para ser utilizado durante as aulas.<br>
+            Etec RGS<br>
+            Feito Por Raphael Pierre, Thiago Russo, Jose Vitor, Gustavo e Ryan Gonçalves <br>
+        </div>
+    </footer>
