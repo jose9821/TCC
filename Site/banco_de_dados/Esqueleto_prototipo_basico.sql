6c947a5bcd935289be03dde2ca863904ceba32ed
Thigas <thiagosrusso@gmail.com>
2026-07-29 15:36:40 -0300

    Update Esqueleto_prototipo_basico.sql

--- Site/banco_de_dados/Esqueleto_prototipo_basico.sql
+++ Site/banco_de_dados/Esqueleto_prototipo_basico.sql
@@ -14,11 +14,11 @@ CREATE TABLE prototipo.alimentos (
     FOREIGN KEY (escola_id) REFERENCES escolas(id) ON DELETE CASCADE -- modelo padrão das tabelas dos alimentos para copiar e colar pelo ID
 ); 
 
-Create table prototipo.usuarios (
+/*Create table prototipo.usuarios (
 ID Int UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
 login Varchar(30),
 senha Varchar(40),
-Primary Key (ID)) ENGINE = InnoDB; -- https://www.devmedia.com.br/criando-um-sistema-de-cadastro-e-login-com-php-e-mysql/37213 link do script que peguei
+Primary Key (ID)) ENGINE = InnoDB; -- https://www.devmedia.com.br/criando-um-sistema-de-cadastro-e-login-com-php-e-mysql/37213 link do script que peguei */
 
 create table tipo_usuario (
 id_tipo int unsigned auto_increment primary key,
