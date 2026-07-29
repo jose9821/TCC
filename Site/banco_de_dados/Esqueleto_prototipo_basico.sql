875559e0aa853cc605a5cf62d57b1d0398a23090
Zezé <josevitorlion@gmail.com>
2026-07-29 14:19:00 -0300

    Modify database schema and add usuarios table
    coloquei a tabela de usuarios e o cadastro, usei de um site externo que coloquei o link nos comentarios e um show table

--- Site/banco_de_dados/Esqueleto_prototipo_basico.sql
+++ Site/banco_de_dados/Esqueleto_prototipo_basico.sql
@@ -1,16 +1,23 @@
+DROP DATABASE IF EXISTS prototipo;
 CREATE DATABASE prototipo;
-
-CREATE TABLE escolas (
+USE prototipo;
+CREATE TABLE prototipo.escolas (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(150) NOT NULL,
     cidade VARCHAR(100) -- modelo padrão das escolas para copiar e colar pelo ID
 );
-
-CREATE TABLE alimentos (
+CREATE TABLE prototipo.alimentos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     escola_id INT NOT NULL,
     nome_produto VARCHAR(100) NOT NULL,
-    preco DECIMAL(10,2) NOT NULL,
     quantidade_estoque INT NOT NULL,
     FOREIGN KEY (escola_id) REFERENCES escolas(id) ON DELETE CASCADE -- modelo padrão das tabelas dos alimentos para copiar e colar pelo ID
-);
+); 
+
+Create table prototipo.usuarios (
+ID Int UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
+login Varchar(30),
+senha Varchar(40),
+Primary Key (ID)) ENGINE = InnoDB; -- https://www.devmedia.com.br/criando-um-sistema-de-cadastro-e-login-com-php-e-mysql/37213 link do script que peguei
+
+SHOW TABLE STATUS FROM prototipo;
