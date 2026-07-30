c5fce11886a9a058e193cb35ccff3c77689eb7ae
Thigas <thiagosrusso@gmail.com>
2026-07-30 14:18:17 -0300

    Update index.php

--- Site do TCC/Site/public/index.php
+++ Site do TCC/Site/public/index.php
@@ -5,6 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RG Eats</title>
+    <link rel="stylesheet" href="CSS%20(Ryan)/style.css">
 </head>
 <body>
     <section class="hero">
