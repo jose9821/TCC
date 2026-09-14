db56577c9cc37ec9244b6c3624cea3ca7fd27d17
Thigas <thiagosrusso@gmail.com>
2026-09-14 14:05:01 -0300

    Update parceiros.php

--- Site/public/parceiros.php
+++ Site/public/parceiros.php
@@ -7,6 +7,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Parceiros - RG Eats</title>
 <link rel="stylesheet" href="CSS%20(Ryan)/style.css">
+<link rel="stylesheet" href="CSS%20(Ryan)/acessibilidade.css">
 <script src="JS%20(Gustavo)/animacao_bolha.js"></script>
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
 
